7f7c487ab4f3e62dd7b069a05788636260528deb
PegasusWang <291374108@qq.com>
2018-05-11 21:51:08 +0800

    Deployed 31796cc with MkDocs version: 0.17.3

--- 08_字典/dict_adt.py
+++ 08_字典/dict_adt.py
@@ -5,9 +5,9 @@
 
 class Array(object):
 
-    def __init__(self, size=32):
+    def __init__(self, size=32, init=None):
         self._size = size
-        self._items = [None] * size
+        self._items = [init] * size
 
     def __getitem__(self, index):
         return self._items[index]
@@ -41,96 +41,88 @@ class Slot(object):
 
 class HashTable(object):
 
-    UNUSED = None    # 没被使用过的槽，作为该类变量的一个单例，下边都是is 判断
-    EMPTY = Slot(None, None)     # 使用过但是被删除的槽
+    UNUSED = None  # 没被使用过
+    EMPTY = Slot(None, None)  # 使用却被删除过
 
     def __init__(self):
-        self._table = Array(7)
+        self._table = Array(8, init=HashTable.UNUSED)   # 保持 2*i 次方
         self.length = 0
 
     @property
     def _load_factor(self):
-        # load factor 超过 2/3 就重新分配空间
+        # load_factor 超过 0.8 重新分配
         return self.length / float(len(self._table))
 
     def __len__(self):
         return self.length
 
-    def _hash1(self, key):
-        """ 计算key的hash值"""
+    def _hash(self, key):
         return abs(hash(key)) % len(self._table)
 
-    def _find_slot(self, key, for_insert=False):
-        """_find_slot
-
-        :param key:
-        :param for_insert: 是否插入，还是仅仅查询
-        :return:  slot index or None
-        """
-        index = self._hash1(key)
-        base_index = index
-        hash_times = 1
+    def _find_key(self, key):
+        index = self._hash(key)
         _len = len(self._table)
+        while self._table[index] is not HashTable.UNUSED:
+            if self._table[index] is HashTable.EMPTY:
+                index = (index*5 + 1) % _len
+                continue
+            elif self._table[index].key == key:
+                return index
+            else:
+                index = (index*5 + 1) % _len
+        return None
 
-        if not for_insert:  # 查找是否存在 key
-            while self._table[index] is not HashTable.UNUSED:
-                if self._table[index] is HashTable.EMPTY:
-                    index = (base_index + hash_times * hash_times) % _len    # 一个简单的二次方探查
-                    continue
-                elif self._table[index].key == key:
-                    return index
-                index = (base_index + hash_times * hash_times) % _len
-                hash_times += 1
-            return None
-        else:
-            while not self._slot_can_insert(index):  # 循环直到找到一个可以插入的槽
-                index = (base_index + hash_times * hash_times) % _len
-                hash_times += 1
-            return index
+    def _find_slot_for_insert(self, key):
+        index = self._hash(key)
+        _len = len(self._table)
+        while not self._slot_can_insert(index):
+            index = (index*5 + 1) % _len
+        return index
 
     def _slot_can_insert(self, index):
         return (self._table[index] is HashTable.EMPTY or self._table[index] is HashTable.UNUSED)
 
-    def __contains__(self, key):   # in operator
-        index = self._find_slot(key, for_insert=False)
+    def __contains__(self, key):  # in operator
+        index = self._find_key(key)
         return index is not None
 
     def add(self, key, value):
-        if key in self:    # key 相同值不一样的时候，用新的值
-            index = self._find_slot(key, for_insert=False)
+        if key in self:
+            index = self._find_key(key)
             self._table[index].value = value
             return False
         else:
-            index = self._find_slot(key, for_insert=True)
+            index = self._find_slot_for_insert(key)
             self._table[index] = Slot(key, value)
             self.length += 1
-            if self._load_factor >= 0.8:    # 注意超过了 阈值 rehashing
+            if self._load_factor >= 0.8:
                 self._rehash()
             return True
 
     def _rehash(self):
         old_table = self._table
-        newsize = len(self._table) * 2 + 1   # 扩大 2*n + 1
-        self._table = Array(newsize)
+        newsize = len(self._table) * 2
+        self._table = Array(newsize, HashTable.UNUSED)
 
         self.length = 0
 
         for slot in old_table:
             if slot is not HashTable.UNUSED and slot is not HashTable.EMPTY:
-                index = self._find_slot(slot.key, for_insert=True)
+                index = self._find_slot_for_insert(slot.key)
                 self._table[index] = slot
                 self.length += 1
 
     def get(self, key, default=None):
-        index = self._find_slot(key, for_insert=False)
+        index = self._find_key(key)
         if index is None:
             return default
         else:
             return self._table[index].value
 
     def remove(self, key):
-        assert key in self, 'keyerror'
-        index = self._find_slot(key, for_insert=False)
+        index = self._find_key(key)
+        if index is None:
+            raise KeyError()
         value = self._table[index].value
         self.length -= 1
         self._table[index] = HashTable.EMPTY
@@ -139,12 +131,12 @@ class HashTable(object):
     def __iter__(self):
         for slot in self._table:
             if slot not in (HashTable.EMPTY, HashTable.UNUSED):
-                yield slot.key   # 和 python dict 一样，默认遍历 key，需要value 的话写个 items() 方法
-
+                yield slot.key
 
 #########################################
 # 上边是从 哈希表章 拷贝过来的代码，我们会直接继承 HashTable 实现 dict
 #########################################
+
 
 class DictADT(HashTable):
 
@@ -152,6 +144,15 @@ class DictADT(HashTable):
         for slot in self._table:
             if slot not in (HashTable.EMPTY, HashTable.UNUSED):
                 yield slot
+
+    def __setitem__(self, key, value):
+        self.add(key, value)
+
+    def __getitem__(self, key):
+        if key not in self:
+            raise KeyError()
+        else:
+            return self.get(key)
 
     def items(self):
         for slot in self._iter_slot():
@@ -169,6 +170,11 @@ class DictADT(HashTable):
 def test_dict_adt():
     import random
     d = DictADT()
+
+    d['a'] = 1
+    assert d['a'] == 1
+    d.remove('a')
+
     l = list(range(30))
     random.shuffle(l)
     for i in l:
@@ -178,3 +184,6 @@ def test_dict_adt():
         assert d.get(i) == i
 
     assert sorted(list(d.keys())) == sorted(l)
+
+
+test_dict_adt()
