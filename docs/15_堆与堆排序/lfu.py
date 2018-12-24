e4acd6126443505f7508d0055c5ae6b70ecbe108
PegasusWang <291374108@qq.com>
2018-12-24 18:49:13 +0800

    add lfu

---
+++ docs/15_堆与堆排序/lfu.py
@@ -0,0 +1,6 @@
+"""
+https://medium.com/@epicshane/a-python-implementation-of-lfu-least-frequently-used-cache-with-o-1-time-complexity-e16b34a3c49b
+https://leetcode.com/problems/lfu-cache/
+
+这里学习下 LRU(least frequently used)，就是当缓存满了之后剔除一个最少使用的 key。
+"""
