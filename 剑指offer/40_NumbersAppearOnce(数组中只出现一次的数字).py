9e4c22401bea275b8fa68d74a1862f08f2b0067e
PegasusWang <291374108@qq.com>
2022-04-27 17:22:49 +0800

    leetcode 260

--- 剑指offer/40_NumbersAppearOnce(数组中只出现一次的数字).py
+++ 剑指offer/40_NumbersAppearOnce(数组中只出现一次的数字).py
@@ -31,6 +31,7 @@ Your algorithm should run in linear runtime complexity. Could you implement it u
 class Solution1:
     def singleNumber(self, nums):
         """
+        类似题：求只出现一次的数字，其他都出现两次。
         :type nums: List[int]
         :rtype: int
         """
@@ -58,10 +59,43 @@ class Solution:
         while single & mask == 0:
             mask = mask << 1
 
-        print(mask,'||||||||||||||||||')
+        print(mask, '||||||||||||||||||')
         left = [i for i in nums if i & mask]
         right = [i for i in nums if not(i & mask)]
         return [get_single_num(left), get_single_num(right)]
+
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        思路：异或。
+
+        136 题做过只出现一次的一个数字，本题有两个只出现一次的数字。
+        核心在于把数组分成两个。怎么分组呢？
+
+        假设只出现一次的数字式 x1,x2，所有元素异或结果是 x。一定有 x=x1^x2。
+        x不能是0，否则x1==x2，就不是只出现一次的数字了。
+        *可以用位运算 x&-x 取出x 二进制位最低位的 1，设其实是第 L 位置。*
+        可以据此分类数组，一组是二进制第 L位置为 0 的数字，一组L 位置为 1的数字。
+        x1,x2会分别出现在两个组中。这样第一组全部异或得到x1, 第二组全部异或得到 x2
+
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        x = 0
+        for num in nums:
+            x ^= num
+
+        low1pos = x & -x  # 这里也可以用移位运算，x&-x 不太好想，类似上边那个解法
+        x1, x2 = 0, 0
+
+        for num in nums:
+            if num & low1pos:
+                x1 ^= num
+            else:
+                x2 ^= num
+
+        return x1, x2
 
 
 def test():
